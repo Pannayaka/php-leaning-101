7caf2b2b3b55c89c8196ea065a6602c37d38f639
jasaipin <pannayaka@gmail.com>
2024-07-23 19:14:44 +0700

    Add Edit
    aierhkeiiofie

--- items/add.php
+++ items/add.php
@@ -3,10 +3,22 @@ include("../koneksi.php");
 if (!$_SESSION['login']){
 	header('Location:../login.php');
 }
-// var_dump($_SESSION['login']);
 
-$selectUser = mysqli_query($db, "SELECT * FROM items");
 $title = "add items";
+if (isset($_POST['item_name'])) {
+	$name = htmlspecialchars($_POST['item_name']);
+	$price = htmlspecialchars($_POST['item_price']);
+	$stock = htmlspecialchars($_POST['item_stock']);
+	$category = htmlspecialchars($_POST['item_category']);
+	$userid = $_SESSION['login']['id_user'];
+	$inputproduct = mysqli_query($db, "INSERT INTO items (item_name, item_price, item_stock, item_category, item_insert_at, item_user_id) VALUES ('$name','$price', '$stock', '$category' ,NOW(), '$userid')");
+	if ($inputproduct == true) {
+		header('Location:index.php');
+	exit();
+}
+
+}
+
 
 ?>
 
@@ -23,33 +35,33 @@ $title = "add items";
           </div>
           <?php endif ?>
 
-			<form class="row gy-1 gx-2 align-items-center mt-5 ">
+			<form  method="POST" action="" class="row gy-1 gx-2 align-items-center mt-5 ">
 			  <div class="col-auto">
 			    <label class="visually-hidden" for="autoSizingInput">Item Name</label>
-			    <input type="text" class="form-control" id="autoSizingInput" placeholder="Item Name">
+			    <input type="text" class="form-control" id="autoSizingInput" placeholder="Item Name" name="item_name">
 			  </div>
 			  <div class="col-auto">
 			    <label class="visually-hidden" for="autoSizingInputGroup">Item Price</label>
 			    <div class="input-group">
 			      <div class="input-group-text">Price $</div>
-			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Price">
+			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Price" name="item_price">
 			    </div>
 			  </div>
 			  <div class="col-auto">
 			    <label class="visually-hidden" for="autoSizingInputGroup">Item Stock</label>
 			    <div class="input-group">
 			      <div class="input-group-text">Stock</div>
-			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Item Stock">
+			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Item Stock" name="item_stock">
 			    </div>
 			  </div>
 
 			  <div class="col-auto">
 			    <label class="visually-hidden" for="autoSizingSelect">Preference</label>
-			    <select class="form-select" id="autoSizingSelect">
-			      <option selected>Category</option>
-			      <option value="1">One</option>
-			      <option value="2">Two</option>
-			      <option value="3">Three</option>
+			    <select class="form-select" id="autoSizingSelect" name="item_category">
+			      <option selected value="">Select</option>
+			      <option value="Phone">Phone</option>
+			      <option value="Laptop">Laptop</option>
+			      <option value="Accessories">Accessories</option>
 			    </select>
 			  </div>
 			  <div class="col-auto ml-5">
--- items/edit.php
+++ items/edit.php
@@ -1,1 +1,79 @@
-edit
+<?php 
+include("../koneksi.php");
+if (!$_SESSION['login']){
+	header('Location:../login.php');
+}
+
+$itemid = $_GET['id'];
+
+$title = "add items";
+if (isset($_POST['item_name'])) {
+	$name = htmlspecialchars($_POST['item_name']);
+	$price = htmlspecialchars($_POST['item_price']);
+	$stock = htmlspecialchars($_POST['item_stock']);
+	$category = htmlspecialchars($_POST['item_category']);
+	$userid = $_SESSION['login']['id_user'];
+	$date = date("Y-m-d H:i:s");
+	$inputproduct = mysqli_query($db, "UPDATE items SET item_name = '$name', item_price = '$price', item_stock = '$stock', item_category = '$category', item_insert_at ='$date', item_user_id = '$userid' WHERE item_id = '$itemid'");
+	if ($inputproduct == true) {
+		header('Location:index.php');
+	exit();
+}
+
+}
+
+
+$dataa = mysqli_query($db, "SELECT * FROM items WHERE item_id = '$itemid'");
+$row = mysqli_fetch_array($dataa);
+
+
+
+?>
+
+
+
+<?php include '../template/head.php'; ?>
+<?php include '../template/menu.php'; ?>
+
+		<div class="container">
+          <?php if (@$_SESSION['sukses']): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              <?php echo $_SESSION['sukses'] ?>
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          <?php endif ?>
+
+			<form  method="POST" action="" class="row gy-1 gx-2 align-items-center mt-5 ">
+			  <div class="col-auto">
+			    <label class="visually-hidden" for="autoSizingInput">Item Name</label>
+			    <input type="text" class="form-control" id="autoSizingInput" placeholder="Item Name" value="<?php echo $row['item_name']?>" name="item_name">
+			  </div>
+			  <div class="col-auto">
+			    <label class="visually-hidden" for="autoSizingInputGroup">Item Price</label>
+			    <div class="input-group">
+			      <div class="input-group-text">Price $</div>
+			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Price" name="item_price" value="<?php echo $row['item_price']?>" >
+			    </div>
+			  </div>
+			  <div class="col-auto">
+			    <label class="visually-hidden" for="autoSizingInputGroup">Item Stock</label>
+			    <div class="input-group">
+			      <div class="input-group-text">Stock</div>
+			      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Item Stock" name="item_stock" value="<?php echo $row['item_stock']?>" >
+			    </div>
+			  </div>
+
+			  <div class="col-auto">
+			    <label class="visually-hidden" for="autoSizingSelect">Preference</label>
+			    <select class="form-select" id="autoSizingSelect" name="item_category" >
+			      <option  value="">Select</option>
+			      <option <?php echo ($row["item_category"] == "Phone")? "selected" : "" ?> value="Phone">Phone</option>
+			      <option <?php echo ($row["item_category"] == "Laptop")? "selected" : "" ?>value="Laptop">Laptop</option>
+			      <option <?php echo ($row["item_category"] == "Accessories")? "selected" : "" ?>value="Accessories">Accessories</option>
+			    </select>
+			  </div>
+			  <div class="col-auto ml-5">
+			    <button type="submit" class="btn btn-primary">Submit</button>
+			  </div>
+			  </div>
+			</form>
